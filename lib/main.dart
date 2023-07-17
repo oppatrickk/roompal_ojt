fe2a7ce0e94b40066fe7ecbcb78833d579f66c51
Sherry-jsp <pjhoannasherry28@gmail.com>
2023-07-17 10:56:16 +0800

    chore(project): trying of git commands

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
       title: 'Roompal OJT',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
         useMaterial3: true,
       ),
       home: const MyHomePage(title: 'Roompal OJT'),
