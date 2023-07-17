620be3d299589412e2c6ec2750c38925b8abca53
Sherry-jsp <pjhoannasherry28@gmail.com>
2023-07-17 16:03:54 +0800

    build(renter page#2): creating a new screen

---
+++ lib/pages/renter/renter_page(2).dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class BookedDetails extends StatelessWidget {
+  const BookedDetails({super.key});
+  static const String id = 'BookedDetails';
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
