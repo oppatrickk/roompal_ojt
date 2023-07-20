97b8069ed0c983713074009db5cdc85e985ab7dc
Sherry-jsp <pjhoannasherry28@gmail.com>
2023-07-20 14:16:38 +0800

    feat(listing_ownerView):added property counter display

--- lib/pages/property_owner/listing_ownersView.dart
+++ lib/pages/property_owner/listing_ownersView.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:roompal_ojt/widgets/const_elements.dart';
 import 'package:roompal_ojt/widgets/widget_elements.dart';
 
 class ListingOwner extends StatelessWidget {
@@ -19,6 +20,7 @@ class ListingOwner extends StatelessWidget {
           padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 10),
           child: Column(
             children: <Widget>[
+              //search bar and filter box
               Row(
                 children: <Widget>[
                   Expanded(
@@ -30,14 +32,70 @@ class ListingOwner extends StatelessWidget {
                   ),
                   Expanded(
                     child: GestureDetector(
-                      child: FilterBox(),
+                      onTap: null,
+                      child: const FilterBox(),
                     ),
                   ),
                 ],
               ),
+              ksizedBoxTextFieldCol,
+              //Property Count
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Flexible(
+                    child: PropertyCount('Total Property', '25'),
+                  ),
+                  Flexible(
+                    child: PropertyCount('Total Views', '109'),
+                  ),
+                ],
+              ),
+              ksizedBoxTextFieldCol,
             ],
           ),
         ),
+      ),
+    );
+  }
+}
+
+class PropertyCount extends StatelessWidget {
+  PropertyCount(
+    this.title,
+    this.count, {
+    super.key,
+  });
+  String title;
+  String count;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      surfaceTintColor: Colors.white,
+      shape: border,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          ksizedBoxTextFieldCol,
+          Text(
+            title,
+            style: const TextStyle(
+              fontSize: 10,
+              fontFamily: 'ProximaNovaAltBold',
+            ),
+            textAlign: TextAlign.center,
+          ),
+          Text(
+            count,
+            style: const TextStyle(
+              fontSize: 40,
+              color: Color(0xFF1C39BB),
+              fontFamily: 'ProximaNovaAltBold',
+            ),
+            textAlign: TextAlign.center,
+          ),
+        ],
       ),
     );
   }
