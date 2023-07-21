dfef775fc2ef5b2fd50f71e9f6cdec4ffd60bb02
Sherry-jsp <pjhoannasherry28@gmail.com>
2023-07-21 09:04:43 +0800

    feat(owner_review_page): adding appbar, searchbar, and card display

--- lib/pages/property_owner/reviews_page.dart
+++ lib/pages/property_owner/reviews_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:roompal_ojt/widgets/const_elements.dart';
+import 'package:roompal_ojt/widgets/widget_elements.dart';
 
 class OwnerReviewPage extends StatelessWidget {
   const OwnerReviewPage({super.key});
@@ -6,6 +8,73 @@ class OwnerReviewPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Image.asset('assets/img/wmB.png'),
+      ),
+      endDrawer: buildSideBar(context),
+      body: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: Container(
+          padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 10),
+          child: Column(
+            children: [
+              //search bar and filter box
+              Row(
+                children: <Widget>[
+                  Expanded(
+                    flex: 4,
+                    child: searchBar(),
+                  ),
+                  const SizedBox(
+                    width: 10,
+                  ),
+                  Expanded(
+                    child: GestureDetector(
+                      onTap: null,
+                      child: const FilterBox(),
+                    ),
+                  ),
+                ],
+              ),
+              ksizedBoxTextFieldCol,
+              //total reviews card
+              Card(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    SizedBox(
+                      width: 140,
+                      child: Image.asset('assets/img/r.png'),
+                    ),
+                    const Column(
+                      children: [
+                        Text(
+                          'Total Reviews',
+                          style: kCardHeaderStyle,
+                        ),
+                        Text(
+                          '1.2k',
+                          style: TextStyle(
+                            fontSize: 40,
+                            color: Color(0xFF1C39BB),
+                            fontFamily: 'ProximaNovaAltBold',
+                          ),
+                          textAlign: TextAlign.center,
+                        ),
+                        Text(
+                          'Growth in reviews on this year',
+                          style: kReviewSmallSubtitle,
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
