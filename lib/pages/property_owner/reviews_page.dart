4e7342b04ea9af2429f83f7d159a6b14ba126db1
Sherry-jsp <pjhoannasherry28@gmail.com>
2023-07-21 08:51:45 +0800

    feat(owner_review_page): initializing files and adding their routes

---
+++ lib/pages/property_owner/reviews_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class OwnerReviewPage extends StatelessWidget {
+  const OwnerReviewPage({super.key});
+  static const String id = 'OwnerReviewPage';
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
