3249b12e18797a273ed37b13757e76db81420475
adrianMadrona <adrianespares.madrona@bicol-u.edu.ph>
2023-07-21 15:28:25 +0800

    fix(login): added top padding

--- lib/pages/user_registration/login_page.dart
+++ lib/pages/user_registration/login_page.dart
@@ -13,96 +13,95 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        body: SingleChildScrollView(
-          child: Column(
-            children: [
-              Container(
-                decoration: BoxDecoration(
-                  color: Color(0xFF1C39BB),
-                  borderRadius: BorderRadius.only(
-                    bottomLeft: Radius.circular(20),
-                    bottomRight: Radius.circular(20),
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              padding: EdgeInsets.only(top: 40),
+              decoration: BoxDecoration(
+                color: Color(0xFF1C39BB),
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(20),
+                  bottomRight: Radius.circular(20),
+                ),
+              ),
+              child: Center(
+                child: Column(children: [
+                  Image.asset(
+                    'assets/img/logo2.png',
+                    height: 100,
+                    width: 120,
                   ),
-                ),
-                child: Center(
-                  child: Column(children: [
-                    Image.asset(
-                      'assets/img/logo2.png',
-                      height: 100,
-                      width: 120,
-                    ),
-                    Text(
-                      'roompal',
-                      style: TextStyle(color: Colors.white, fontFamily: 'ProximaNovaBold', fontSize: 50),
-                    )
-                    // Image.asset(
-                    //   'assets/img/bW.png',
-                    //   height: 80,
-                    //   width: 400,
-                    // ),
-                  ]),
-                ),
+                  Text(
+                    'roompal',
+                    style: TextStyle(color: Colors.white, fontFamily: 'ProximaNovaBold', fontSize: 50),
+                  )
+                  // Image.asset(
+                  //   'assets/img/bW.png',
+                  //   height: 80,
+                  //   width: 400,
+                  // ),
+                ]),
               ),
-              Container(
-                padding: const EdgeInsets.all(20),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: <Widget>[
-                    Text(
-                      'Login',
-                      style: kRoomName,
-                      textAlign: TextAlign.center,
+            ),
+            Container(
+              padding: const EdgeInsets.all(20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: <Widget>[
+                  Text(
+                    'Login',
+                    style: kRoomName,
+                    textAlign: TextAlign.center,
+                  ),
+                  kSizedBox,
+                  Container(
+                    padding: const EdgeInsets.all(10),
+                    decoration: boxDecoration(),
+                    child: Column(
+                      children: <Widget>[
+                        textField(label: 'Email'),
+                        ksizedBoxTextFieldCol,
+                      ],
                     ),
-                    kSizedBox,
-                    Container(
-                      padding: const EdgeInsets.all(10),
-                      decoration: boxDecoration(),
-                      child: Column(
-                        children: <Widget>[
-                          textField(label: 'Email'),
-                          ksizedBoxTextFieldCol,
-                        ],
-                      ),
+                  ),
+                  kSizedBox,
+                  Container(
+                    padding: const EdgeInsets.all(10),
+                    decoration: boxDecoration(),
+                    child: Column(
+                      children: <Widget>[
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.stretch,
+                          children: <Widget>[
+                            textField(label: 'Password'),
+                            ksizedBoxTextFieldCol,
+                          ],
+                        ),
+                      ],
                     ),
-                    kSizedBox,
-                    Container(
-                      padding: const EdgeInsets.all(10),
-                      decoration: boxDecoration(),
-                      child: Column(
-                        children: <Widget>[
-                          Column(
-                            crossAxisAlignment: CrossAxisAlignment.stretch,
-                            children: <Widget>[
-                              textField(label: 'Password'),
-                              ksizedBoxTextFieldCol,
-                            ],
-                          ),
-                        ],
-                      ),
+                  ),
+                  kSizedBox,
+                  Container(
+                    padding: const EdgeInsets.all(10),
+                    decoration: boxDecoration(),
+                    child: Column(
+                      children: <Widget>[
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.stretch,
+                          children: <Widget>[
+                            textField(label: 'Description'),
+                            ksizedBoxTextFieldCol,
+                          ],
+                        ),
+                      ],
                     ),
-                    kSizedBox,
-                    Container(
-                      padding: const EdgeInsets.all(10),
-                      decoration: boxDecoration(),
-                      child: Column(
-                        children: <Widget>[
-                          Column(
-                            crossAxisAlignment: CrossAxisAlignment.stretch,
-                            children: <Widget>[
-                              textField(label: 'Description'),
-                              ksizedBoxTextFieldCol,
-                            ],
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
+                  ),
+                ],
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
