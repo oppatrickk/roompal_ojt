d7dd5ac91bb333144fd952e5960db485de1afd3f
Sherry-jsp <pjhoannasherry28@gmail.com>
2023-07-25 14:21:22 +0800

    feat(popUp): adding new types of popups

--- lib/widgets/pop_up.dart
+++ lib/widgets/pop_up.dart
@@ -26,13 +26,59 @@ class PopUpTemporary extends StatelessWidget {
             ElevatedButton(
               onPressed: () {
                 showDialog(
+                  barrierDismissible: false,
                   context: context,
                   builder: (BuildContext context) {
-                    return CustomDialogButton();
+                    return CustomDialogButton1(
+                      'Property Listing',
+                      'Submit your Listing?',
+                    );
                   },
                 );
               },
-              child: Text('Sample 2'),
+              child: Text('Listing'),
+            ),
+            ksizedBoxTextFieldCol,
+            ElevatedButton(
+              onPressed: () {
+                showDialog(
+                  barrierDismissible: false,
+                  context: context,
+                  builder: (BuildContext context) {
+                    return CustomDialogButton1(
+                      'Payment',
+                      'Confirm your Payment?',
+                    );
+                  },
+                );
+              },
+              child: Text('Payment'),
+            ),
+            ksizedBoxTextFieldCol,
+            ElevatedButton(
+              onPressed: () {
+                showDialog(
+                  barrierDismissible: false,
+                  context: context,
+                  builder: (BuildContext context) {
+                    return const CustomDialogButton2('Thank You for Trusting Roompal!');
+                  },
+                );
+              },
+              child: Text('Thank You'),
+            ),
+            ksizedBoxTextFieldCol,
+            ElevatedButton(
+              onPressed: () {
+                showDialog(
+                  barrierDismissible: false,
+                  context: context,
+                  builder: (BuildContext context) {
+                    return const CustomDialogButton3('Thank You for Trusting Roompal!');
+                  },
+                );
+              },
+              child: Text('Success'),
             ),
           ],
         ),
@@ -51,14 +97,202 @@ class DialogButton extends StatelessWidget {
       content: Text('something something'),
       actions: <Widget>[
         TextButton(onPressed: () {}, child: Text('Okay')),
-        TextButton(onPressed: () {}, child: Text('Cancel')),
+        TextButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            child: Text('Cancel')),
       ],
     );
   }
 }
 
-class CustomDialogButton extends StatelessWidget {
-  const CustomDialogButton({super.key});
+class CustomDialogButton3 extends StatelessWidget {
+  const CustomDialogButton3(this.content, {super.key});
+  final String content;
+
+  @override
+  Widget build(BuildContext context) {
+    return Dialog(
+      surfaceTintColor: Colors.white,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(4),
+      ),
+      child: SizedBox(
+        height: 180,
+        child: Stack(
+          clipBehavior: Clip.none,
+          alignment: AlignmentDirectional.topCenter,
+          children: [
+            Column(
+              children: <Widget>[
+                Container(
+                  width: 300,
+                  decoration: const BoxDecoration(
+                    borderRadius: BorderRadius.only(
+                      topRight: Radius.circular(4),
+                      topLeft: Radius.circular(4),
+                    ),
+                    color: Color(0xFF1C39BB),
+                  ),
+                  padding: const EdgeInsets.all(12),
+                  height: 50,
+                  child: Column(
+                    children: [
+                      //sparkle icons/ photo
+                    ],
+                  ),
+                ),
+                Container(
+                  padding: const EdgeInsets.only(
+                    left: 20,
+                    right: 20,
+                    top: 25,
+                    bottom: 5,
+                  ),
+                  child: Text(
+                    content,
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 17,
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
+                const Divider(
+                  indent: 20,
+                  endIndent: 20,
+                  thickness: 1,
+                  color: Color(0xFFF1F1F1),
+                ),
+                SizedBox(
+                  height: 20,
+                  width: 70,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    style: ElevatedButton.styleFrom(
+                      elevation: 0,
+                      surfaceTintColor: Colors.white,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                    ),
+                    child: const Text(
+                      'Ok',
+                      style: TextStyle(color: Color(0xFFFEB618)),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Container(
+              padding: EdgeInsets.all(10),
+              child: CircleAvatar(
+                backgroundColor: Color(0xFF6CE679),
+                radius: 30,
+                child: Icon(
+                  Icons.check,
+                  size: 50,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class CustomDialogButton2 extends StatelessWidget {
+  const CustomDialogButton2(this.content, {super.key});
+  final String content;
+
+  @override
+  Widget build(BuildContext context) {
+    return Dialog(
+      surfaceTintColor: Colors.white,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(4),
+      ),
+      child: SizedBox(
+        height: 180,
+        child: Column(
+          children: <Widget>[
+            Container(
+              width: 300,
+              decoration: const BoxDecoration(
+                borderRadius: BorderRadius.only(
+                  topRight: Radius.circular(4),
+                  topLeft: Radius.circular(4),
+                ),
+                color: Color(0xFF1C39BB),
+              ),
+              padding: const EdgeInsets.all(12),
+              height: 50,
+              child: Column(
+                children: [
+                  //sparkle icons/ photo
+                ],
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.only(
+                left: 20,
+                right: 20,
+                top: 20,
+                bottom: 5,
+              ),
+              child: Text(
+                content,
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 17,
+                  color: Colors.black,
+                ),
+              ),
+            ),
+            const Divider(
+              indent: 20,
+              endIndent: 20,
+              thickness: 1,
+              color: Color(0xFFF1F1F1),
+            ),
+            SizedBox(
+              height: 20,
+              width: 70,
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                style: ElevatedButton.styleFrom(
+                  elevation: 0,
+                  surfaceTintColor: Colors.white,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                ),
+                child: const Text(
+                  'Ok',
+                  style: TextStyle(color: Color(0xFFFEB618)),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class CustomDialogButton1 extends StatelessWidget {
+  const CustomDialogButton1(this.title, this.content, {super.key});
+  final String title;
+  final String content;
 
   @override
   Widget build(BuildContext context) {
@@ -81,9 +315,9 @@ class CustomDialogButton extends StatelessWidget {
               ),
               padding: const EdgeInsets.all(12),
               height: 50,
-              child: const SizedBox.expand(
+              child: SizedBox.expand(
                 child: Text(
-                  'Property Listing',
+                  title,
                   textAlign: TextAlign.center,
                   style: TextStyle(
                     fontWeight: FontWeight.bold,
@@ -100,8 +334,8 @@ class CustomDialogButton extends StatelessWidget {
                 top: 20,
                 bottom: 2,
               ),
-              child: const Text(
-                'Submit your Listing?',
+              child: Text(
+                content,
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 16,
@@ -117,12 +351,14 @@ class CustomDialogButton extends StatelessWidget {
               color: Color(0xFFF1F1F1),
             ),
             Row(
-              crossAxisAlignment: CrossAxisAlignment.center,
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
                 ElevatedButton(
-                  onPressed: () {},
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
                   style: ElevatedButton.styleFrom(
+                    padding: const EdgeInsets.symmetric(horizontal: 35),
                     surfaceTintColor: Colors.white,
                     shape: RoundedRectangleBorder(
                       side: const BorderSide(
@@ -140,6 +376,7 @@ class CustomDialogButton extends StatelessWidget {
                 ElevatedButton(
                   onPressed: () {},
                   style: ElevatedButton.styleFrom(
+                    padding: const EdgeInsets.symmetric(horizontal: 35),
                     surfaceTintColor: Colors.white,
                     shape: RoundedRectangleBorder(
                       side: const BorderSide(
